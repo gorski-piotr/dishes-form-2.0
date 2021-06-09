6ac23391d5ef39dd3a44655b484f0661d04015d5
Piotr Gorski <gorskipiotr5@gmail.com>
2021-06-09 16:20:16 +0100

    NewDish displaying edited

--- src/components/NewDish.js
+++ src/components/NewDish.js
@@ -1,12 +1,24 @@
 function NewDish(props) {
   //   console.log("props", props);
   return (
-    <div className="text-green-600 font-bold">
-      Congratulations! You have created a new dish!
+    <div className="text-green-600">
+      <p className="font-bold">Congratulations! You have created a new dish!</p>
       <ul>
         <li>Name: {props.newDish.name}</li>
         <li>Preparation time: {props.newDish.preparation_time}</li>
         <li>Type: {props.newDish.type}</li>
+        {props.newDish.type === "pizza" && (
+          <>
+            <li>Slices of pizza: {props.newDish.no_of_slices}</li>
+            <li>Diameter: {props.newDish.diameter}</li>
+          </>
+        )}
+        {props.newDish.type === "soup" && (
+          <li>Spiciness scale: {props.newDish.spiciness_scale}</li>
+        )}
+        {props.newDish.type === "sandwich" && (
+          <li>Slices of bread: {props.newDish.slices_of_bread}</li>
+        )}
       </ul>
     </div>
   );
